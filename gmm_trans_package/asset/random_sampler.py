5c1283efa4859cb51a86edd7d932ebcd92d8dd7a
Weijie Xia <weijiexia@tudelft.nl>
2024-08-13 16:37:54 +0200

    update dataloader

--- gmm_trans_package/asset/random_sampler.py
+++ gmm_trans_package/asset/random_sampler.py
@@ -4,6 +4,8 @@ def random_sample(data, strategy, random_sampe=20, num_samples_all=250):
     if strategy == 'random':
         _sample_indx = np.random.choice(range(num_samples_all), random_sampe, replace=False)
         _sample_indx = np.sort(_sample_indx)
+        _sample_indx = _sample_indx.astype(np.int32)
+        
         _data = data[:, _sample_indx, :]
         
     if strategy == 'top':
