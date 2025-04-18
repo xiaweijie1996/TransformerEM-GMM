500bb5d44d3d063b19820da3402ba4c772288c3d
Weijie Xia <wxia@int6.local.snellius.surf.nl>
2025-04-18 21:14:13 +0200

    add

--- exp/exp_second_round/usa_load_15minutes/train_merge.py
+++ exp/exp_second_round/usa_load_15minutes/train_merge.py
@@ -45,8 +45,8 @@ para_dim = n_components*2
 hidden_d = 96*4
 out_d = 96
 n_heads = 2
-mlp_ratio = 2
-n_blocks = 3
+mlp_ratio = 1
+n_blocks = 2
 encoder = gmm_model.ViT_encodernopara(chw, hidden_d, out_d, n_heads, mlp_ratio, n_blocks).to(device)
 _model_scale = sum(p.numel() for p in encoder.parameters() if p.requires_grad)
 print('number of parameters: ', _model_scale)
