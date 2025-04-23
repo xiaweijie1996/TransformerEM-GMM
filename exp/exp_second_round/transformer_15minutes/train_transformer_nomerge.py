a73d7e29a5da6001e18ea3f53f0a2512a9471a55
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-04-23 18:37:52 +0200

    switch em

--- exp/exp_second_round/transformer_15minutes/train_transformer_nomerge.py
+++ exp/exp_second_round/transformer_15minutes/train_transformer_nomerge.py
@@ -10,9 +10,9 @@ import numpy as np
 import torch.optim as optim
 
 import model.gmm_transformer as gmm_model
-import asset.gmm_train_tool_improved as gmm_train_tool
+import asset.gmm_train_tool as gmm_train_tool
 import asset.em_pytorch as ep
-import asset.plot_eva as pae
+import asset.plot_eva as pa 
 from asset.dataloader import Dataloader_nolabel
 
 torch.set_default_dtype(torch.float64)
