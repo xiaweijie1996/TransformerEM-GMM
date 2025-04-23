532c92c20e24523758086c68e883806415a5d97a
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-04-23 17:32:06 +0200

    try

--- exp/exp_second_round/timesnet_mse_userload_15minutes/train_timesnet_30mr.sh
+++ exp/exp_second_round/timesnet_mse_userload_15minutes/train_timesnet_30mr.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name="15m_transformer_merge"
+#SBATCH --job-name="15m_userload_merge"
 #SBATCH --partition=gpu_a100
 #SBATCH --time=24:00:00
 #SBATCH -N 1
 #SBATCH --gpus=1
-#SBATCH --mail-type=END
 #SBATCH --mail-user=w.xia@tudelft.nl
 
 
@@ -14,7 +13,6 @@ module load Miniconda3/23.5.2-0
 
 source $HOME/TransformerEM-GMM/.venv/bin/activate
 
+cp $HOME/TransformerEM-GMM/exp/data_process_for_data_collection_all/new_data_15minute_grid_merge.pkl "$TMPDIR"
 
-cp $HOME/TransformerEM-GMM/exp/data_process_for_data_collection_all/transformer_data_15minutes.pkl "$TMPDIR"
-
-python $HOME/TransformerEM-GMM/exp/exp_second_round/timesnet_mse_transformer_15minutes/timesnet.py "$TMPDIR/transformer_data_15minutes.pkl"
+python $HOME/TransformerEM-GMM/exp/exp_second_round/timesnet_mse_userload_15minutes/timesnet.py "$TMPDIR/new_data_15minute_grid_merge.pkl"
