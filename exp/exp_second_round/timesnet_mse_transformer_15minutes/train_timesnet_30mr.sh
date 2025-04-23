efd5da38bb98fe65e949ed2ca8979c683adad9c6
Weijie Xia <wxia@int4.local.snellius.surf.nl>
2025-04-23 17:23:59 +0200

    check

--- exp/exp_second_round/timesnet_mse_transformer_15minutes/train_timesnet_30mr.sh
+++ exp/exp_second_round/timesnet_mse_transformer_15minutes/train_timesnet_30mr.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name="15m-transformer_timesnet_mse"
+#SBATCH --job-name="15m_transformer_merge"
 #SBATCH --partition=gpu_a100
 #SBATCH --time=24:00:00
 #SBATCH -N 1
 #SBATCH --gpus=1
-#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-type=END
 #SBATCH --mail-user=w.xia@tudelft.nl
 
-module load 2023r1 
-module load cuda/11.6
+
+mmodule load 2023
+module load Miniconda3/23.5.2-0
 
 source $HOME/TransformerEM-GMM/.venv/bin/activate
+
 
 cp $HOME/TransformerEM-GMM/exp/data_process_for_data_collection_all/transformer_data_15minutes.pkl "$TMPDIR"
 
