355fb10da495e3eb67f5752f2837350763a8b9a6
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-04-26 18:53:48 +0200

    add

--- exp/exp_second_round/eva/eveload/sample_from_mainmodel.py
+++ exp/exp_second_round/eva/eveload/sample_from_mainmodel.py
@@ -15,14 +15,38 @@ import asset.gmm_train_tool as gmm_train_tool
 import asset.em_pytorch as ep
 import asset.plot_eva as plot_eva
 
+import asset.timesnet_loader as timesloader
+import exp_second_round.timesnet_mse_userload_15minutes.timesnet_utils as ut
+from exp_second_round.timesnet_mse_userload_15minutes.timesnet_config import TimesBlockConfig 
+import exp_second_round.timesnet_mse_userload_15minutes.timesnet_train as tt
+import exp_second_round.timesnet_mse_userload_15minutes.timesnet as timesnet
 
 # Set default dtype to float64
 torch.set_default_dtype(torch.float64)
-
 # Determine the device to use
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+#%%
+"Load TimesNet mse model"
+# check the model
+configs = TimesBlockConfig()
+model_timesnetmse = timesnet.Model(configs).to(device)
 
+# print number of parameters
+num_params = sum(p.numel() for p in model_timesnetmse.parameters())
+print(num_params)
+
+data_path = 'exp/data_process_for_data_collection_all/new_data_15minute_grid_nomerge.pkl'
+data_loader = timesloader.TimesNetLoader(data_path, 
+                                            batch_size=60, 
+                                            split_ration=(0.8, 0.1, 0.1),
+                                            full_length=366)
+
+model_path = 'exp/exp_second_round/timesnet_mse_userload_15minutes/30mr_timesnet_1281536.pth'
+model_timesnetmse.load_state_dict(torch.load(model_path, map_location=device))
+
+#%%
+"Load Transformer model"
 # Define the encoder parameters
 random_sample_num = 96
 n_components = 6    
@@ -33,32 +57,26 @@ out_d = 96
 n_heads = 1
 mlp_ratio = 1
 n_blocks = 2
-
 # Create the encoder model
 encoder = gmm_model.ViT_encodernopara(chw, hidden_d, out_d, n_heads, mlp_ratio, n_blocks).to(device)
-
 # load state dict of the model
 model_path = 'exp/exp_second_round/user_load_15minutes/model/loadmerge_encoder_96_704366.pth'
 model = torch.load(model_path, map_location=device)
 encoder.load_state_dict(model)
-
 embedding_para = torch.nn.Embedding(n_components*2, 1).to(device)
 emb_empty_token = torch.nn.Embedding(1, chw[2]).to(device)
-
 path_embedding = 'exp/exp_second_round/user_load_15minutes/model/loadmerge_embedding_96_704366.pth'
 emb_weight = torch.load(path_embedding, map_location=device, weights_only=False)
 path_empty = 'exp/exp_second_round/user_load_15minutes/model/loadmerge_emb_empty_token_96_704366.pth'
 empty_token_vec = torch.load(path_empty, map_location=device,weights_only=False)
-
 # load data
-batch_size =  1
+batch_size =  2
 split_ratio = (0.8,0.1,0.1)
 data_path =  'exp/data_process_for_data_collection_all/new_data_15minute_grid_merge.pkl' ## 
 dataset = Dataloader_nolabel(data_path,  batch_size=batch_size
                     , split_ratio=split_ratio)
-print('lenthg of test data: ', dataset.__len__()*split_ratio[1])
-test_data = dataset.load_test_data(batch_size )
-
+test_data = dataset.load_test_data(batch_size)
+print('test_data shape: ', test_data.shape)
 # normalize the input data
 min_test_data = test_data[:,:, :-1].min(axis=1).reshape(batch_size , 1, chw[2]-1)
 max_test_data = test_data[:,:, :-1].max(axis=1).reshape(batch_size , 1, chw[2]-1)
@@ -69,6 +87,9 @@ test_data = torch.tensor(test_data, dtype=torch.float64).to(device)
 # _random_num = 32 # torch.randint(1, random_sample_num+1, (1,)).item() # Number of the shots
 
 for _random_num in [4, 8, 16, 32]:
+    
+    "Transform the data to GMM parameters"
+    encoder.eval()
     _test_sample_part = rs.random_sample(test_data , 'random', _random_num)
     _test_sample_part[:, :, -1] = _test_sample_part[:, :, -1]/365 # simple data embedding
     _test_sample_part_emb = gmm_train_tool.pad_and_embed(_test_sample_part, random_sample_num, _random_num,
@@ -86,12 +107,27 @@ for _random_num in [4, 8, 16, 32]:
 
     mean = _new_para[:, :n_components* 96].view(-1, n_components, 96)
     cov = _new_para[:, n_components* 96:].view(-1, n_components, 96)
-
     # recover the scale
     recovered_test_data = test_data[:, :, :-1].clone() * (max_test_data -min_test_data+1e-15) + min_test_data
     recover_test_sample_part = _test_sample_part[:, :, :-1].clone() * (max_test_data -min_test_data+1e-15) + min_test_data
     recover_test_sample_part = _test_sample_part[:, :, :-1].clone()
 
+    "TimesNet model"
+    full_series, index_mask = data_loader.load_test_data_times(test_data)
+    test_data_timenetmse, random_mask, scaler = tt.normalize_and_mask_fix(full_series, index_mask, device, _random_num)
+    y_hat = model_timesnetmse(test_data_timenetmse.double(), None, random_mask.double())
+    
+    # Scale back to original
+    y_hat = y_hat * (scaler[1] - scaler[0]) + scaler[0]
+    
+    mask_bt = 1- index_mask.sum(dim=2) 
+    mask_bt = mask_bt > 0
+    y_filtered_per_sample = [
+        y_hat[i, mask_bt[i], :]           # Tensor of shape [Z_i, F]
+        for i in range(y_hat.shape[0])
+    ]
+    
+    "evaluate the model"
     mmd = 0
     kl = 0
     ks = 0
@@ -104,6 +140,12 @@ for _random_num in [4, 8, 16, 32]:
     ws_partreal = 0
     msem_partreal = 0
     
+    mmd_timesnetmse = 0
+    kl_timesnetmse = 0
+    ks_timesnetmse = 0
+    ws_timesnetmse = 0
+    msem_timesnetmse = 0
+
 
     for i in tqdm(range(batch_size)):
         # sample from the GMM
@@ -125,15 +167,22 @@ for _random_num in [4, 8, 16, 32]:
         ws_partreal += wasserstein_distance(samples.flatten(), _part_real.flatten())
         msem_partreal = plot_eva.calculate_autocorrelation_mse(samples, _part_real)
         
+        # TimesNet mse
+        _part_real_timesnetmse = y_filtered_per_sample[_num].cpu().detach().numpy()
+        mmd_timesnetmse += plot_eva.compute_mmd(samples, _part_real_timesnetmse)
+        kl_timesnetmse += plot_eva.compute_kl_divergence(samples, _part_real_timesnetmse)
+        ks_timesnetmse += ks_2samp(samples.flatten(), _part_real_timesnetmse.flatten())[0]
+        ws_timesnetmse += wasserstein_distance(samples.flatten(), _part_real_timesnetmse.flatten())
+        msem_timesnetmse = plot_eva.calculate_autocorrelation_mse(samples, _part_real_timesnetmse)
+        # print the shape of the samples
         
         # Plot the samples
         plt.figure(figsize=(10, 6))
-        plt.subplot(2, 1, 1)
+        plt.subplot(3, 1, 1)
 
         # plot the samples the colors indicate the sum of the samples
         samples = samples * (max_test_data[_num] -min_test_data[_num]+1e-15) + min_test_data[_num]
         for i in range(samples.shape[0]):
-            print('samples shape check: ', samples.shape)
             _sum = samples[i, :-1].sum()
             color = plt.cm.viridis(_sum / test_data.max())
             plt.plot(samples[i, :-1], alpha=0.05, c=color)
@@ -141,7 +190,7 @@ for _random_num in [4, 8, 16, 32]:
         plt.xlabel('Time')
         plt.ylabel('Value')
 
-        plt.subplot(2, 1, 2)
+        plt.subplot(3, 1, 2)
         # plot the real samples
         for i in range(test_data.shape[1]):
             _sum = test_data[_num, i, :-1].sum()
@@ -150,22 +199,33 @@ for _random_num in [4, 8, 16, 32]:
         plt.title('real')
         plt.xlabel('Time')
         plt.ylabel('Value')
+        
+        plt.subplot(3, 1, 3)
+        # plot the timesnet mse samples
+        for i in range(_part_real_timesnetmse.shape[0]):
+            _sum = _part_real_timesnetmse[i, :-1].sum()
+            color = plt.cm.viridis(_sum / test_data.max())
+            plt.plot(_part_real_timesnetmse[i, :-1], alpha=0.05, c=color)
+        plt.title('TimesNet mse')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.tight_layout()
+        
         plt.savefig(f'exp/exp_second_round/eva/eveload/plot/samples_from_gmm_{_random_num}.png')
-        plt.show()
+        plt.close()
 
 
+        # print(f'mmd of {_random_num}-shots: ', mmd/batch_size)
+        # print(f'kl of {_random_num}-shots: ', kl/batch_size)
+        # print(f'ks of {_random_num}-shots: ', ks/batch_size)
+        # print(f'ws of {_random_num}-shots: ', ws/batch_size)
+        # print(f'msem of {_random_num}-shots: ', msem/batch_size)
 
-        print(f'mmd of {_random_num}-shots: ', mmd/batch_size)
-        print(f'kl of {_random_num}-shots: ', kl/batch_size)
-        print(f'ks of {_random_num}-shots: ', ks/batch_size)
-        print(f'ws of {_random_num}-shots: ', ws/batch_size)
-        print(f'msem of {_random_num}-shots: ', msem/batch_size)
-
-        print(f'mmd_partreal of {_random_num}-shots: ', mmd_partreal/batch_size)
-        print(f'kl_partreal of {_random_num}-shots: ', kl_partreal/batch_size)
-        print(f'ks_partreal of {_random_num}-shots: ', ks_partreal/batch_size)
-        print(f'ws_partreal of {_random_num}-shots: ', ws_partreal/batch_size)
-        print(f'msem_partreal of {_random_num}-shots: ', msem_partreal/batch_size)
+        # print(f'mmd_partreal of {_random_num}-shots: ', mmd_partreal/batch_size)
+        # print(f'kl_partreal of {_random_num}-shots: ', kl_partreal/batch_size)
+        # print(f'ks_partreal of {_random_num}-shots: ', ks_partreal/batch_size)
+        # print(f'ws_partreal of {_random_num}-shots: ', ws_partreal/batch_size)
+        # print(f'msem_partreal of {_random_num}-shots: ', msem_partreal/batch_size)
         
         # save the results in a text file
         # with open('exp/exp_second_round/eva/eveload/sample_from_gmm.txt', 'a') as f:
