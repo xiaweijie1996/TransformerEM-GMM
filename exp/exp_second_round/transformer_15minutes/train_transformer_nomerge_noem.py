26d8fa15cb7a7c0489f88c74f747115d3c19308e
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-06-04 00:47:42 +0200

    add

--- exp/exp_second_round/transformer_15minutes/train_transformer_nomerge_noem.py
+++ exp/exp_second_round/transformer_15minutes/train_transformer_nomerge_noem.py
@@ -59,7 +59,7 @@ emb_empty_token = torch.nn.Embedding(1, chw[2]).to(device)
 
 # define the optimizer and loss function and cyclic learning rate scheduler
 optimizer = optim.AdamW(list(encoder.parameters()), lr=lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0001, amsgrad=False)
-scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=5e-5, max_lr=1e-3, step_size_up=sub_epoch*2, mode='triangular', cycle_momentum=False)
+scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=5e-5, max_lr=lr, step_size_up=1000, mode='triangular', cycle_momentum=False)
 
 # log number of parameters of encoder and decoder
 wandb.init(project=f'transformer_{random_sample_num}_train_nomerge')
@@ -69,40 +69,41 @@ wandb.log({'num_parameters_encoder': _model_scale})
 mid_loss = 100000
 
 for epoch in range(num_epochs):
-    for j in range(2):
-        # train.model
-        encoder.train()
+   
+    # train.model
+    encoder.train()
+    embedding_para.train()
+    emb_empty_token.train()
+    
+    # _loss, _new_para, _param, train_sample[:, :, :-1], _train_sample_part[:, n_components*2:, :-1], var, (_train_min, _train_max) 
+    _loss, _random_num, _new_para, _param, r_samples, r_samples_part, _mm = gmm_train_tool.get_loss_le(dataset, encoder,
+                                                                            random_sample_num, min_random_sample_num, n_components, 
+                                                                            embedding_para, emb_empty_token, 'True', device)
+    optimizer.zero_grad()
+    _loss.backward()
+    optimizer.step()
+    scheduler.step()
         
-        # _loss, _new_para, _param, train_sample[:, :, :-1], _train_sample_part[:, n_components*2:, :-1], var, (_train_min, _train_max) 
+    wandb.log({'loss_train': _loss.item(), 'random_num': _random_num, 'epoch': epoch, 'lr': optimizer.param_groups[0]['lr']})
+    print('epoch: ', epoch, 'loss_test: ', _loss.item(), 'random_num: ', _random_num)
+    
+    if epoch % 100 == 0:
+        encoder.eval()
+        embedding_para.eval()
+        emb_empty_token.eval()
         _loss, _random_num, _new_para, _param, r_samples, r_samples_part, _mm = gmm_train_tool.get_loss_le(dataset, encoder,
-                                                                              random_sample_num, min_random_sample_num, n_components, 
-                                                                              embedding_para, emb_empty_token, 'True', device)
-        optimizer.zero_grad()
-        _loss.backward()
-        optimizer.step()
-        scheduler.step()
+                                                                            random_sample_num, min_random_sample_num, n_components, 
+                                                                            embedding_para, emb_empty_token, 'False', device)
         
-        # if epoch % 1 == 0:
-        #     encoder.eval()
-        #     wandb.log({'loss_train': _loss.item()})
-        #     _loss_collection = []
-        #     for _ in range(20):
-        #         _loss, _random_num, _new_para, _param, r_samples, r_samples_part, _mm = gmm_train_tool.get_loss_le(dataset, encoder,
-        #                                                                             random_sample_num, min_random_sample_num, n_components, 
-        #                                                                             embedding_para, emb_empty_token, 'False', device)
-        #         _loss_collection.append(_loss)
-        #     _loss = torch.stack(_loss_collection).mean()
         
-        print('epoch: ', epoch, 'loss_test: ', _loss.item(), 'random_num: ', _random_num)
         wandb.log({'loss_test': _loss.item(), 'random_num': _random_num, 'epoch':epoch})
         
-        # save the model and embeding
-        if _loss.item() < mid_loss:
-            mid_loss = _loss.item()
-            torch.save(encoder.state_dict(), save_model + f'transformer_encoder_{random_sample_num}_{_model_scale}.pth')
-            torch.save(embedding_para, save_model + f'transformer_embedding_{random_sample_num}_{_model_scale}.pth')
-            torch.save(emb_empty_token, save_model + f'transformer_emb_empty_token_{random_sample_num}_{_model_scale}.pth')
-
-    if epoch % 100 == 0:
         save_path = save_image+f'_{_model_scale}.png'
         llk_e = pa.plot_samples(save_path, batch_size, n_components, _mm, _new_para, r_samples, r_samples_part, _param, figsize=(10, 15))
+    
+    # save the model and embeding
+    if _loss.item() < mid_loss:
+        mid_loss = _loss.item()
+        torch.save(encoder.state_dict(), save_model + f'transformer_encoder_{random_sample_num}_{_model_scale}.pth')
+        torch.save(embedding_para, save_model + f'transformer_embedding_{random_sample_num}_{_model_scale}.pth')
+        torch.save(emb_empty_token, save_model + f'transformer_emb_empty_token_{random_sample_num}_{_model_scale}.pth')
